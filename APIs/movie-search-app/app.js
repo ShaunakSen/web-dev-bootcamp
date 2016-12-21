fdae0a3af006afc693c5ca66c6bfb3ae674e5462
shaunak1105 <shaunak1105@gmail.com>
2016-12-21 12:42:29 +0000

    request module2

--- APIs/movie-search-app/app.js
+++ APIs/movie-search-app/app.js
@@ -1,5 +1,22 @@
 var express = require("express");
 var app = express();
+var request = require("request");
+var uri = "https://www.omdbapi.com/?s=harry";
+app.set("view engine", "ejs");
+
+app.get("/results", function(req, res){
+    // res.send("hi..."); 
+    
+    // trigger api call
+    
+    request(uri, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
+        }
+    })
+    
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
