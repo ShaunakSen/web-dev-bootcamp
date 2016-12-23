e600fad400ed671e6bc7605c68bbb787d5258030
ShaunakSen <shaunak1105@gmail.com>
2016-12-24 01:03:25 +0530

    data persistence to campgrounds route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,38 +1,65 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-var campgrounds = [
-    {
-        name: "Salmon Creek",
-        image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-    },
-    {
-        name: "Granite Hill",
-        image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress"
-    },
-    {
-        name: "Mountain Goat's Rest",
-        image: "https://images.pexels.com/photos/27679/pexels-photo-27679.jpg?h=350&auto=compress"
-    },
-    {
-        name: "Salmon Creek",
-        image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-    },
-    {
-        name: "Granite Hill",
-        image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress"
-    },
-    {
-        name: "Mountain Goat's Rest",
-        image: "https://images.pexels.com/photos/27679/pexels-photo-27679.jpg?h=350&auto=compress"
-    }
-];
+// Setting up our schema
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+
+var CampGround = mongoose.model("Campground", campgroundSchema);
+
+/*
+ CampGround.create({
+ name: "Granite Hill",
+ image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress"
+ }, function (err, campground) {
+ if (err) {
+ console.log(err)
+ } else {
+ console.log("Created campground");
+ console.log(campground);
+ }
+ });
+ */
+
+// var campgrounds = [
+//     {
+//         name: "Salmon Creek",
+//         image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
+//     },
+//     {
+//         name: "Granite Hill",
+//         image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress"
+//     },
+//     {
+//         name: "Mountain Goat's Rest",
+//         image: "https://images.pexels.com/photos/27679/pexels-photo-27679.jpg?h=350&auto=compress"
+//     },
+//     {
+//         name: "Salmon Creek",
+//         image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
+//     },
+//     {
+//         name: "Granite Hill",
+//         image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress"
+//     },
+//     {
+//         name: "Mountain Goat's Rest",
+//         image: "https://images.pexels.com/photos/27679/pexels-photo-27679.jpg?h=350&auto=compress"
+//     }
+// ];
 
 app.set("view engine", "ejs");
 
@@ -41,7 +68,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // res.render("campgrounds", {campgrounds: campgrounds});
+    CampGround.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log("Error...", err)
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds})
+        }
+    })
 });
 
 app.post("/campgrounds", function (req, res) {
@@ -49,10 +83,15 @@ app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    //  redirect to campgrounds
-
-    res.redirect("/campgrounds");
+    // Create new campground and save to db
+    CampGround.create(newCampground, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //  redirect to campgrounds
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function (req, res) {
