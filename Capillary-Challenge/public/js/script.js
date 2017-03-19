ad7d5fffa767f132485cdad0b824bf14b93ed3ae
Shaunak Sen <shaunak1105@gmail.com>
2017-03-19 15:39:25 +0530

    accessing all the games data in angular controller

---
+++ Capillary-Challenge/public/js/script.js
@@ -0,0 +1,13 @@
+var myApp = angular.module('capillaryApp', []);
+myApp.config(function ($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+});
+
+myApp.controller('MainController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
+    
+    $scope.gamesData = $window.gameInfo;
+
+    console.log("Games data in angular is", $scope.gamesData)
+}]);
+
--- Capillary-Challenge/readme.txt
+++ Capillary-Challenge/readme.txt
@@ -2,3 +2,5 @@
 2. Set up an Express app
 3. Build up the API
 4. Style landing page
+5. get the data via angular
+5. Build Games-cards
