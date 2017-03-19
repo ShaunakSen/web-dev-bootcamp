9be6f637169aecc1d7ca80b53fb652db674dfc42
Shaunak Sen <shaunak1105@gmail.com>
2017-03-19 20:19:43 +0530

    implemented pagination system

--- Capillary-Challenge/public/js/script.js
+++ Capillary-Challenge/public/js/script.js
@@ -7,7 +7,22 @@ myApp.config(function ($httpProvider) {
 myApp.controller('MainController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
     
     $scope.gamesData = $window.gameInfo;
-    
+
+
+    $scope.startingIndex = 0;
+    $scope.endIndex = 10;
+    $scope.pageNumber = 1;
+
+    $scope.nextPage = function (pageNumber) {
+        $scope.pageNumber = pageNumber;
+        $scope.startingIndex = 10*(pageNumber-1);
+        $scope.endIndex = $scope.startingIndex + 10;
+    };
+
+    $scope.generateArray = function (number) {
+        return new  Array(number);
+    } ;
+
 
     console.log("Games data in angular is", $scope.gamesData)
 }]);
--- Capillary-Challenge/readme.txt
+++ Capillary-Challenge/readme.txt
@@ -1,6 +1,5 @@
 1. Import csv file into local db
 2. Set up an Express app
 3. Build up the API
-4. Style landing page
-5. get the data via angular
-5. Build Games-cards
+
+6.stars as well
