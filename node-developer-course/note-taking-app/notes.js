722ea5cefe25e6b59edcc49e81348367c89aaa24
shaunak1105 <shaunak1105@gmail.com>
2017-05-25 23:19:22 +0530

    adding a new note

--- node-developer-course/note-taking-app/notes.js
+++ node-developer-course/note-taking-app/notes.js
@@ -1,8 +1,32 @@
 console.log("starting notes");
-
+const fs = require('fs');
 
 var addNote = (title, body) => {
-  console.log("Adding note", title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  // fetch notes
+
+  try{
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  }catch(err){
+
+  }
+
+  // Check for duplicate titles
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0){
+    notes.push(note);
+    // update the file
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+
 };
 
 var getAll = () => {
